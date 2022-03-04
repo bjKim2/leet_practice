ed2191592eeb0ca9c2095b34ea6c400256f673a7
bjKim2 <rlaxodjs3@naver.com>
2022-03-04 10:05:18 +0900

    ..

--- python/leet4_prac1.py
+++ python/leet4_prac1.py
@@ -7,8 +7,24 @@ def ifswitch(m,n):
         n = tmp
     return m,n
 
-
-
+def balancing(m_ind,n_ind):
+    if (m_ind > len(m)-1):
+        bal = m_ind - len(m)-1
+        m_ind = len(m) - 1
+        n_ind += bal
+    if (m_ind < 0):
+        bal = 0 - m_ind
+        m_ind = 0
+        n_ind -= bal
+    if (n_ind > len(n)-1):
+        bal = n_ind -len(n)-1
+        n_ind = len(n) - 1
+        m_ind += bal
+    if (n_ind < 0):
+        bal = 0 - n_ind
+        n_ind = 0
+        m_ind -= bal
+    return  m_ind,n_ind
 m = [1,3,5,7,9]
 n = [6,8,10,12,13]
 
@@ -23,12 +39,41 @@ m,n = ifswitch(m,n)
 # 전체 개수가 짝수 일 떄 2개 값을 찾아 나눔
 # 전체 개수가 홀수일 떄
 
-m_index = len(m) //2
-n_index = len(n) //2
-print(m_index)
+all_ind = (len(m) +len(n))-1//2
 
+n_ind = len(n) //2
+m_ind = all_ind -n_ind
+i = 2
+while(True):
+    if(m[m_ind] > n[n_ind]):
+        m_ind -= all_ind//i
+        n_ind += all_ind//i
+        m_ind, n_ind =balancing(m_ind,n_ind)
+    if(m[m_ind] < n[n_ind]):
+        m_ind += all_ind//i
+        n_ind -= all_ind//i 
+        m_ind, n_ind =balancing(m_ind,n_ind)
 
+    if(all_ind//i == 0):
+        break
+    
+    i*=2    
 
+if ((len(m) +len(n))% 2 == 0):
+    if(len(m)  == len(n)):
+        resuilt = (m[m_ind] +n[n_ind])/2
+    else:
+        if(n_ind == len(n) -1 or n_ind == 0):
+            result = (m[m_ind] +m[m_ind+1])/2
+        else:
+            result = (m[m_ind] +n[n_ind])/2
+else:
+    if(m[m_ind] > n[n_ind]):
+        result = m[m_ind]
+    else:
+        result = n[n_ind]
+        
+    
 
 
 # 함수가 크면 바꾸는 재귀 함수로 설정
@@ -37,3 +82,7 @@ print(m_index)
 
 
 
+
+class Solution(object):
+    
+    c
